ec69847b438f654ac7b7804ecac4c5fdea8be296
Mike Neumegen <mike@cloudcannon.com>
2016-02-21 13:31:30 -0800

    fixed up missing js library

--- js/layout.js
+++ js/layout.js
@@ -155,20 +155,20 @@ $(".theme-demo").click(function () {
 }());
 
 $(document).ready(function () {
-	$(".before-after-slider").twentytwenty();
-	$(function() {
-		$('a[href*="#"]:not([href="#"])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html, body').animate({
-						scrollTop: target.offset().top
-					}, 600);
-					return false;
-				}
+	if (jQuery().twentytwenty) {
+		$(".before-after-slider").twentytwenty();
+	}
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 600);
+				return false;
 			}
-		});
+		}
 	});
 
 	$(".slideshow").each(function () {
