1fba972cc830ed3364885172b5b328db2c66c2dc
Mike Neumegen <mike@cloudcannon.com>
2016-09-01 14:15:54 +1200

    removed extra images

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,9 @@ module.exports = function(grunt) {
 			options: {
 				reference: 'img/',
 				directory: ['_site/**/*.html'],
-				days: 30,
-				remove: false, // set to true to delete unused files from project
-				reportOutput:'report.txt', // set to false to disable file output
+				days: false,
+				remove: true, // set to true to delete unused files from project
+				reportOutput: false, // set to false to disable file output
 				fail: false // set to true to make the task fail when unused files are found
 			}
 		}
