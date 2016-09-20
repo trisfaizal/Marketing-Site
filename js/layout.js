3270103845830ca8c3a735018ad9c6a289503121
Mike Neumegen <mike@cloudcannon.com>
2016-09-21 11:16:41 +1200

    fix hero image snap

--- js/layout.js
+++ js/layout.js
@@ -323,7 +323,7 @@ $(document).ready(function () {
 		var $this = $(this),
 			videoId = /^https:\/\/www\.youtube\.com\/watch\?v\=([a-zA-Z1-9]+)$/.exec($this.attr('href'))[1];
 
-	$this.parent().html('<div class="video-embed"><iframe class="screenshot" src="https://www.youtube.com/embed/' + videoId + '?autoplay=1&modestbranding=1&rel=0" frameborder="0" allowfullscreen></iframe></div>');
+	$this.parent().parent().html('<div class="video-embed"><iframe class="screenshot" src="https://www.youtube.com/embed/' + videoId + '?autoplay=1&modestbranding=1&rel=0" frameborder="0" allowfullscreen></iframe></div>');
 
 		return false;
 	});
