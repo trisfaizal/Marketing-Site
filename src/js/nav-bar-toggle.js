15f0fa27fecb72ce678662f573b0b9343dcb56f3
Ryan Collins <ryan@cloudcannon.com>
2019-03-05 23:17:44 +0000

    Updated 'src/js/nav-bar-toggle.js' via CloudCannon

--- src/js/nav-bar-toggle.js
+++ src/js/nav-bar-toggle.js
@@ -1,26 +1,26 @@
 let navBar = document.querySelector(".doc-nav");
 let navButton = document.getElementById("features-nav-button");
 
-if (!document.body.classList.contains("feature-nav-open")) {
+if (document.body.classList.contains("feature-nav-open")) {
+    navButton.classList.add("arrow-up");
+}
+
+function open() {
+    document.body.classList.add("feature-nav-open");
     navButton.classList.add("arrow-up");
 }
 
 function close() {
-    document.body.classList.add("feature-nav-open");
-    navButton.classList.remove("arrow-up");
-}
-
-function open() {
     document.body.classList.remove("feature-nav-open");
-    navButton.classList.add("arrow-up");
+    navButton.classList.remove("arrow-up");
 }
 
 navButton.addEventListener("click", function (event) {
     if (window.innerWidth > 930) {
         if (document.body.classList.contains("feature-nav-open")) {
-            open();
-        } else {
             close();
+        } else {
+            open();
         }
 
         event.preventDefault();
